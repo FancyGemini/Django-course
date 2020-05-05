abf993bed4870871381603d64bdb0df1da2cb44b
keytoon <zitong945@gmail.com>
2020-05-05 20:10:21 +0800

    模型迁移并给django管理界面添加了相应的管理界面

--- pr1/background/admin.py
+++ pr1/background/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from background.models import Student, Teacher, Course, Classroom
 
 # Register your models here.
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    # 显示哪些属性
+    list_display = ('sid', 'sname', 'age', 'gender')
+    # 哪些可以点进去
+    list_display_links = ('sid', 'sname')
+    
+# 注册其他模型
+admin.site.register([Teacher, Course, Classroom])
