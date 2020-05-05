adabfb20e10607eeb2909cf448d2333f4a307e12
FancyGemini <fancying001@gmail.com>
2020-05-05 17:26:43 +0800

    更改 models.py, mysql->sqlite3

--- pr1/pr1/settings.py
+++ pr1/pr1/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    'background'
+    'background',
 ]
 
 MIDDLEWARE = [
@@ -77,12 +76,14 @@ WSGI_APPLICATION = 'pr1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django',
-        'USER': 'root',
-        'PASSWORD': 'Fancy123',
-        'HOST': '127.0.0.1',
-        'PORT': '3306'
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': 'django',
+#        'USER': 'root',
+#        'PASSWORD': 'Fancy123',
+#        'HOST': '127.0.0.1',
+#        'PORT': '3306'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
