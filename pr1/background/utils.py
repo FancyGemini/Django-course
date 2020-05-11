b4189c413e5199ec4b54a3162aa2e39ca048b2d2
keytoon <zitong945@gmail.com>
2020-05-11 15:11:10 +0800

    将方法函数放入utils.py中

---
+++ pr1/background/utils.py
@@ -0,0 +1,81 @@
+from background import models
+import datetime
+
+def check_cookies(request):
+    is_stu = True
+    v = request.COOKIES.get('log_s')
+    if not v:
+        v = request.COOKIES.get('log_t')
+        if not v:
+            return False
+        else:
+            is_stu = False
+    return True
+
+def parse_UUID(uuid):
+    """ 解析UUID 去掉-\n
+    :param uuid: 需要解析的UUID\n
+    :return: str\n
+    """
+    uuid = str(uuid)
+    uuid_str = ''.join(uuid.split('-'))
+    return uuid_str
+
+# 创建课程签到信息函数
+def set_sign(cid, time_start, time_end, debug=False):
+    """ 创建课程签到信息函数\n
+    :param cid: 课程编号\n
+    :param time_start: 开始时间\n
+    :param time_end: 结束时间\n
+    :return: 创建失败返回NoneType 创建成功返回课程签到信息主键
+    """
+    course = models.Course.objects.get(cid=cid)
+    cousign_info = models.CouSignInfo.objects.filter(cid__cid=cid, timeend__gt=time_start)
+    # 测试用
+    if debug:
+        return models.CouSignInfo.objects.create(cid=course, timestart=time_start, timeend=time_end)
+
+    # 如果开始时间大于结束时间 或 还有正在进行中的签到 不予继续创建签到
+    if time_start > time_end or cousign_info.exists():
+        return None
+
+    cousign = models.CouSignInfo.objects.create(cid=course, timestart=time_start, timeend=time_end)
+    return cousign
+
+# 学生签到函数
+def student_sign(stuid, couid, cousignid, debug=False):
+    """ 学生签到函数\n
+    :param stuid: 学生学号\n
+    :param couid: 课程编号\n
+    :param cousignid: 签到UUID\n
+    :return: 返回学生签到状态字典
+    """
+    signInfo = {
+        'isOutTime' : True,     # 是否在签到时间段
+        'isInClass' : False,    # 是否为本课程学生
+        'isSigned' : False      # 是否已经签到
+    }
+    
+    try:
+        stu = models.StuToCourse.objects.get(sid__sid=stuid, cid__cid=couid)
+        signInfo['isInClass'] = True
+        
+        # 测试用
+        if debug:
+            models.SignInfo.objects.create(sid=stu.sid, cid=stu.cid)
+            return signInfo
+        
+        if models.SignInfo.objects.filter(cid=cousignid).exists():
+            signInfo['isSigned'] = True
+            return signInfo
+        
+        # 获取课程详细信息
+        time = datetime.datetime.now()
+        if time >= cousignid.timestart and time <= cousignid.timeend :
+            # 写入签到信息
+            models.SignInfo.objects.create(sid=stu.sid, cid=stu.cid)
+            signInfo['isOutTime'] = False
+    except Exception:
+        pass
+    
+    return signInfo
--- pr1/background/views.py
+++ pr1/background/views.py
@@ -11,6 +11,7 @@ from django.core.exceptions import ObjectDoesNotExist
 import qrcode
 import arrow
 import datetime
+from background import utils as u
 # import pyzbar.pyzbar as pyzbar
 # import cv2
 
@@ -19,21 +20,8 @@ def home(request):
     return render(request, 'home.html')
 
 
-def check_cookies(request):
-    is_stu = True
-    v = request.COOKIES.get('log_s')
-    if not v:
-        v = request.COOKIES.get('log_t')
-        if not v:
-            return False
-        else:
-            is_stu = False
-    return True
-
-
-
 def signin(request):
-    if check_cookies(request):
+    if u.check_cookies(request):
         v = request.COOKIES.get('log_s')
         if not v:
             v = request.COOKIES.get('log_t')
@@ -88,11 +76,6 @@ def logout(request):
     re.delete_cookie('log_t')
     return re
 
-def parse_UUID(uuid):
-    uuid = str(uuid)
-    uuid_str = ''.join(uuid.split('-'))
-    return uuid_str
-
 # 测试用
 def class_test(request):
     class_list = models.Course.objects.all().values('cid', 'cname', 'tid__tname')
@@ -113,20 +96,6 @@ def classform(request, cid):
     context = { 'cid' : cid }
     return render(request, 'class_form.html', context)
 
-# 创建课程签到信息函数
-def set_sign(cid, time_start, time_end, debug=False):
-    course = models.Course.objects.get(cid=cid)
-    cousign_info = models.CouSignInfo.objects.filter(cid__cid=cid, timeend__gt=time_start)
-    # 测试用
-    if debug:
-        return models.CouSignInfo.objects.create(cid=course, timestart=time_start, timeend=time_end)
-
-    # 如果开始时间大于结束时间 或 还有正在进行中的签到 不予继续创建签到
-    if time_start > time_end or cousign_info.exists():
-        return None
-
-    cousign = models.CouSignInfo.objects.create(cid=course, timestart=time_start, timeend=time_end)
-    return cousign
 
 # 教师发布签到页面
 # 还需要加入权限判断 只有老师能访问该页面 目前测试阶段暂不加
@@ -139,51 +108,20 @@ def publish_sign(request, cid):
         context['starttime'] = utc.datetime
         utc = arrow.get(request.POST['end']+":00-Asia/Shanghai", tfmt)
         context['endtime'] = utc.datetime
-        context['cousign'] = set_sign(cid, context['starttime'], context['endtime'])
+        context['cousign'] = u.set_sign(cid, context['starttime'], context['endtime'])
         context['host'] = request.get_host()
     return render(request, 'publish_test.html', context)
 
-# 学生签到函数
-def student_sign(stuid, couid, cousignid, debug=False):
-    signInfo = {
-        'isOutTime' : True,     # 是否在签到时间段
-        'isInClass' : False,    # 是否为本课程学生
-        'isSigned' : False      # 是否已经签到
-    }
-    
-    try:
-        stu = models.StuToCourse.objects.get(sid__sid=stuid, cid__cid=couid)
-        signInfo['isInClass'] = True
-        
-        # 测试用
-        if debug:
-            models.SignInfo.objects.create(sid=stu.sid, cid=stu.cid)
-            return signInfo
-        
-        if models.SignInfo.objects.filter(cid=cousignid).exists():
-            signInfo['isSigned'] = True
-            return signInfo
-        
-        # 获取课程详细信息
-        time = datetime.datetime.now()
-        if time >= cousignid.timestart and time <= cousignid.timeend :
-            # 写入签到信息
-            models.SignInfo.objects.create(sid=stu.sid, cid=stu.cid)
-            signInfo['isOutTime'] = False
-    except Exception:
-        pass
-    
-    return signInfo
 
 # 学生签到界面
 def sign_page(request, cousign):
     sid = str(request.COOKIES.get('log_s'))
-    cou = parse_UUID(cousign)
+    cou = u.parse_UUID(cousign)
     context = {}
     try:
         stu = models.Student.objects.get(sid = sid)
         cousignid = models.CouSignInfo.objects.get(id = cou)
-        context = student_sign(str(sid), cousignid.cid.cid, cou)
+        context = u.student_sign(str(sid), cousignid.cid.cid, cou)
         request.session['signinfo'] = context
         request.session['signflag'] = True
         print(request.session['signflag'])
@@ -196,7 +134,7 @@ def sign_page(request, cousign):
         
 # 获取对应签到二维码
 def get_qrcode(request, cousign):
-    cousignid = parse_UUID(cousign)
+    cousignid = u.parse_UUID(cousign)
     try:
         cou = models.CouSignInfo.objects.get(id = cousignid)
         img = qrcode.make('http://' + request.get_host() + '/student/sign/' + str(cou.id))
